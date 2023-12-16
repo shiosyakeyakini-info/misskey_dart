98a64794302b30eadaa8cc88979673e1dd73135c
poppingmoon <63451158+poppingmoon@users.noreply.github.com>
2023-12-16 09:29:01 +0900

    remove displayName

--- lib/src/enums/reaction_acceptance.dart
+++ lib/src/enums/reaction_acceptance.dart
@@ -1,10 +1,6 @@
 enum ReactionAcceptance {
-  likeOnlyForRemote("全て（リモートはいいねのみ）"),
-  nonSensitiveOnly("非センシティブのみ"),
-  nonSensitiveOnlyForLocalLikeOnlyForRemote("非センシティブのみ（リモートはいいねのみ）"),
-  likeOnly("いいねのみ");
-
-  const ReactionAcceptance(this.displayName);
-
-  final String displayName;
+  likeOnlyForRemote,
+  nonSensitiveOnly,
+  nonSensitiveOnlyForLocalLikeOnlyForRemote,
+  likeOnly,
 }
