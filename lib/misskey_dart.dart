1357ec7f478a1626e7863cb34be4e6af6346faab
sorairo <sorairo@shiosyakeyakini.info>
2024-01-28 17:18:04 +0900

    misskey_dart.dart 修正

--- lib/misskey_dart.dart
+++ lib/misskey_dart.dart
@@ -173,6 +173,7 @@ export 'src/data/users/users_search_request.dart';
 export 'src/data/users/users_users_request.dart';
 export 'src/data/users/users_update_memo_request.dart';
 export 'src/data/users/users_pages_request.dart';
+export 'src/data/users/users_get_frequently_replied_users_request.dart';
 export 'src/data/block/block_create_request.dart';
 export 'src/data/block/block_delete_request.dart';
 export 'src/data/renote_mute/renote_mute_create_request.dart';
