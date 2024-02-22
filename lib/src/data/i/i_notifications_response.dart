0d28b3e7906ec893d710d00201868de6faa42bb5
poppingmoon <63451158+poppingmoon@users.noreply.github.com>
2024-02-23 03:06:19 +0900

    グルーピングされた通知に対応

--- lib/src/data/i/i_notifications_response.dart
+++ lib/src/data/i/i_notifications_response.dart
@@ -25,6 +25,8 @@ class INotificationsResponse with _$INotificationsResponse {
     UserLite? user,
     Note? note,
     RolesListResponse? role,
+    List<INotificationsReaction>? reactions,
+    List<UserLite>? users,
     UserExportableEntities? exportedEntity,
     String? fileId,
     String? message,
@@ -32,6 +34,17 @@ class INotificationsResponse with _$INotificationsResponse {
 
   factory INotificationsResponse.fromJson(Map<String, Object?> json) =>
       _$INotificationsResponseFromJson(json);
+}
+
+@freezed
+class INotificationsReaction with _$INotificationsReaction {
+  const factory INotificationsReaction({
+    required UserLite user,
+    required String reaction,
+  }) = _INotificationsReaction;
+
+  factory INotificationsReaction.fromJson(Map<String, Object?> json) =>
+      _$INotificationsReactionFromJson(json);
 }
 
 enum UserExportableEntities {
